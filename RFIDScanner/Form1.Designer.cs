a79e2c17af783b849f70f2329149589c417ec74e
ITitan Dev <manhpcpro@gmail.com>
2017-09-27 01:25:49 +0700

    Upload RFIDScanner Project

---
+++ RFIDScanner/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace RFIDScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboComports = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelMSSV = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.labelRoom = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ports";
+            // 
+            // cboComports
+            // 
+            this.cboComports.FormattingEnabled = true;
+            this.cboComports.Location = new System.Drawing.Point(80, 10);
+            this.cboComports.Name = "cboComports";
+            this.cboComports.Size = new System.Drawing.Size(121, 21);
+            this.cboComports.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(233, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(314, 10);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 3;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(395, 10);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(109, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(408, 363);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelMSSV
+            // 
+            this.labelMSSV.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelMSSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMSSV.Location = new System.Drawing.Point(542, 323);
+            this.labelMSSV.Name = "labelMSSV";
+            this.labelMSSV.Size = new System.Drawing.Size(516, 73);
+            this.labelMSSV.TabIndex = 12;
+            this.labelMSSV.Text = "MSSV";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(1087, 119);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(105, 254);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // labelRoom
+            // 
+            this.labelRoom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelRoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRoom.Location = new System.Drawing.Point(542, 203);
+            this.labelRoom.Name = "labelRoom";
+            this.labelRoom.Size = new System.Drawing.Size(516, 73);
+            this.labelRoom.TabIndex = 14;
+            this.labelRoom.Text = "Room";
+            // 
+            // labelName
+            // 
+            this.labelName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(542, 102);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(516, 73);
+            this.labelName.TabIndex = 15;
+            this.labelName.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 487);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelRoom);
+            this.Controls.Add(this.labelMSSV);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cboComports);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboComports;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelMSSV;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label labelRoom;
+        private System.Windows.Forms.Label labelName;
+    }
+}
+
---
+++ RFIDScanner/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Data.SqlClient;
+
+
+namespace RFIDScanner
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextCallbackCom(string text);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnRefresh_Click(0, EventArgs.Empty);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(cboComports.Text))
+            {
+                MessageBox.Show("Chọn một cổng COM", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+                
+            try
+            {
+                serialPort.PortName = cboComports.Text;
+                serialPort.BaudRate = 9600;
+                serialPort.Open();
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                btnRefresh.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("Connection fail!!!!","Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnRefresh_Click(0,EventArgs.Empty);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+                btnRefresh.Enabled = true;
+                return;
+            }
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            btnRefresh.Enabled = true;
+        }
+        String InputDataCom = "";
+        String allIDCard = "";
+        String currentIDCard = "";
+        int currentIndex = 0;
+        Regex RegexFindLast = new Regex("\\[\\d{8}(\\d?)(\\d?)\\]\\z");
+        Match m;
+        private void SetTextCom(string text)
+        {
+            richTextBox1.Text += text;
+            m = RegexFindLast.Match(allIDCard);
+            if (m.Success && (m.Value != currentIDCard || allIDCard.LastIndexOf(currentIDCard) != currentIndex))
+            {
+                currentIDCard = m.Value;
+                updateInfo();
+                Console.Beep(4000, 200);
+                currentIndex = allIDCard.LastIndexOf(currentIDCard);
+            }
+        }
+
+        
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            InputDataCom = serialPort.ReadExisting();
+            allIDCard += InputDataCom;
+            if (InputDataCom != String.Empty)
+            {
+                this.BeginInvoke(new SetTextCallbackCom(SetTextCom), new object[] { InputDataCom });
+            }
+            
+        }
+        
+        private void updateInfo()
+        {
+            switch (currentIDCard)
+            {
+                default:
+                    labelName.Text = currentIDCard;
+                    break;
+            }
+
+        }
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            cboComports.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+                cboComports.Items.Add(port);
+            if (ports.Length > 0)
+                cboComports.SelectedIndex = 0;
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            btnRefresh.Enabled = true;
+        }
+    }
+}
