4e58d7b239d360e1117623b5e710003756d9a65b
ITitan Dev <manhpcpro@gmail.com>
2017-11-07 06:32:43 +0700

    Reset QuanLyLuuTruKTX

--- QuanLyLuuTruKTX/dbo.DotDienNuoc.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[DotDienNuoc] (
-    [MaDot]       INT  IDENTITY (1, 1) NOT NULL,
-    [NgayBatDau]  DATE NOT NULL,
-    [NgayKetThuc] DATE NOT NULL, 
-    [DonGiaDien] INT NOT NULL, 
-    [DonGiaNuoc] INT NULL
-);
-
